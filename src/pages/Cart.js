0b788eb40d18a9a90d6dfddb374d6203cb0398ec
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2025-01-11 16:04:49 +0400

    I finished working with numbers

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -14,9 +14,11 @@ const Cart = () => {
     return retrieving;
   });
 
-  let sum = getItemLocalStorage.reduce((Starting, EachElement) => {
-    return Starting + EachElement.price;
-  }, 0);
+  let sum = getItemLocalStorage
+    .reduce((Starting, EachElement) => {
+      return Starting + (parseFloat(EachElement.price) || 0);
+    }, 0)
+    .toFixed(2);
 
   const DeletHendler = (index) => {
     let NewStorage = [];
@@ -29,8 +31,10 @@ const Cart = () => {
     let updatedStorage = [...getItemLocalStorage];
     if (index >= 0 && updatedStorage[index].total < 10) {
       updatedStorage[index].total = updatedStorage[index].total + 1;
-      updatedStorage[index].price =
-        updatedStorage[index].price + updatedStorage[index].initialPrice;
+      updatedStorage[index].price = (
+        parseFloat(updatedStorage[index].price) +
+        parseFloat(updatedStorage[index].initialPrice)
+      ).toFixed(2);
     }
     setGetItemLocalStorage(updatedStorage);
     localStorage.setItem("Key", JSON.stringify(updatedStorage));
@@ -40,8 +44,10 @@ const Cart = () => {
     let updatedStorage = [...getItemLocalStorage];
     if (index >= 0 && updatedStorage[index].total > 1) {
       updatedStorage[index].total = updatedStorage[index].total - 1;
-      updatedStorage[index].price =
-        updatedStorage[index].price - updatedStorage[index].initialPrice;
+      updatedStorage[index].price = (
+        parseFloat(updatedStorage[index].price) -
+        parseFloat(updatedStorage[index].initialPrice)
+      ).toFixed(2);
     }
     setGetItemLocalStorage(updatedStorage);
     localStorage.setItem("Key", JSON.stringify(updatedStorage));
@@ -54,26 +60,28 @@ const Cart = () => {
           <div className="test2">
             {getItemLocalStorage.map((element, index) => (
               <div className="StoreMainBox">
-                <div key={index} className="storageBox">
-                  <img src={element.prPhoto}></img>
-                  <p>{element.prName}</p>
-                  <button
-                    onClick={() => {
-                      DecrimnetHendler(index);
-                    }}
-                  >
-                    -
-                  </button>
-                  {element.total}
-                  <button
-                    onClick={() => {
-                      IncrimentHendler(index);
-                    }}
-                  >
-                    +
-                  </button>
+                <div key={index} className="storageBox1">
+                  <div className="storageBox">
+                    <img src={element.prPhoto}></img>
+                    <p className="ElementName">{element.prName}</p>
+                    <button
+                      onClick={() => {
+                        DecrimnetHendler(index);
+                      }}
+                    >
+                      -
+                    </button>
+                    <div className="TotalItem">{element.total}</div>
+                    <button
+                      onClick={() => {
+                        IncrimentHendler(index);
+                      }}
+                    >
+                      +
+                    </button>
 
-                  <p className="localPrace">{element.price}</p>
+                    <p className="localPrace">{element.price}</p>
+                  </div>
                   <button
                     onClick={() => {
                       DeletHendler(index);
